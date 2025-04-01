0dfd7ff9d1ce4f120e0a9656e33bba55641ded84
aliquidmi <allkamironova1424@gmail.com>
2025-04-01 21:28:00 +0300

    Fourth commit

--- lesson3/lesson3.3.py
+++ lesson3/lesson3.3.py
@@ -1,14 +1,14 @@
 lst_1 = []
-lst1_1 = lst_1[0:((len(lst_1) + 1) // 2)]
-lst2_1 = lst_1[((len(lst_1) + 1) // 2):(len(lst_1) + 1)]
+lst1_1 = lst_1[:((len(lst_1) + 1) // 2)]
+lst2_1 = lst_1[((len(lst_1) + 1) // 2):]
 print(lst1_1, lst2_1)
 
 lst_2 = [0, 1, 2, 3, 4, 5]
-lst1_2 = lst_2[0:((len(lst_2) + 1) // 2)]
-lst2_2 = lst_2[((len(lst_2) + 1) // 2):(len(lst_2) + 1)]
+lst1_2 = lst_2[:((len(lst_2) + 1) // 2)]
+lst2_2 = lst_2[((len(lst_2) + 1) // 2):]
 print(lst1_2, lst2_2)
 
 lst_3 = [0, 1, 2, 3, 4]
-lst1_3 = lst_3[0:((len(lst_3) + 1) // 2)]
-lst2_3 = lst_3[((len(lst_3) + 1) // 2):(len(lst_3) + 1)]
+lst1_3 = lst_3[:((len(lst_3) + 1) // 2)]
+lst2_3 = lst_3[((len(lst_3) + 1) // 2):]
 print(lst1_3, lst2_3)
